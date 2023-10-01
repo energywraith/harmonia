270ea1d4ebda65f3a1520153183b87e46805fb87
Adam Jałocha <jalochadev@gmail.com>
2023-10-01 17:07:49 +0200

    Added text-shadow utility class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import plugin from "tailwindcss/plugin";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -13,6 +14,11 @@ const config: Config = {
       lobster: "var(--lobster-font)",
     },
     extend: {
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
       colors: {
         primary: {
           DEFAULT: "#734B5E",
@@ -35,6 +41,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
 export default config;

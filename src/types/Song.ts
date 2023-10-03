7da2728d93dcc72ff2575ce42a2544163f3e74d4
Adam Jałocha <jalochadev@gmail.com>
2023-10-03 17:41:27 +0200

    Added Album to Song Type

--- src/types/Song.ts
+++ src/types/Song.ts
@@ -1,7 +1,10 @@
+import { Album } from ".";
+
 interface Song {
   id: string;
   title: string;
   header_image_url: string;
+  album: Album;
   primary_artist: {
     id: string;
     name: string;
